667f3e29ed3a1aea0bf0cab45d33c6c8b62d6ad1
Maximiliano Vicentin <maximilianoarielvicentin@gmail.com>
2023-05-01 16:51:48 -0300

    feat: Modified from default component

---
+++ src/components/movieHome/index.tsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { IMovie } from "../../types";
+import Pagination from "../pagination/Pagination";
+import { MovieContext } from "../../context/MovieContext";
+import MovieCard from "../movieCard/MovieCard";
+import SearchMovie from "../searchMovie/SearchMovie";
+import { Wrapper } from "./StyledIndex";
+
+export default function MovieHome() {
+  const { movies }: any = useContext(MovieContext);
+  return (
+    <div>
+      <SearchMovie/>
+        <Wrapper>
+          {movies?.map((value: IMovie) => {
+        return <MovieCard key={value.id} {...value} />;
+      })}
+        </Wrapper>
+      
+      <Pagination />
+    </div>
+  );
+}
