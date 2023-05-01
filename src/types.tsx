35785e7b5e647835afa2dc05bd80635fc73ccf27
Maximiliano Vicentin <maximilianoarielvicentin@gmail.com>
2023-05-01 16:09:04 -0300

    feat: Add new types component

---
+++ src/types.tsx
@@ -0,0 +1,11 @@
+export interface IMovie {
+  id: number;
+  title: string;
+  backdrop_path: string;
+  original_language: string;
+  vote_average: string;
+  vote_count: number;
+  overview: string;
+  poster_path: string;
+  release_date:string;
+}
