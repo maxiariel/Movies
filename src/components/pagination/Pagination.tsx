6f9a60573b1a8a4833d29fc51c00859f9d6f28d8
Maximiliano Vicentin <maximilianoarielvicentin@gmail.com>
2023-05-02 15:20:19 -0300

    feat: pagination is succesful

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,30 +1,68 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { MovieContext } from "../../context/MovieContext";
+import { Wrapper, PageButton, PaginationArrowButton } from "./StyledPagination";
 
 export default function Pagination() {
-  const { totalPages, currentPage, setCurrentPage }: any = useContext(MovieContext);
+  const { totalPages, currentPage, setCurrentPages }: any = useContext(MovieContext);
 
+  function handleClickNext() {
+    setCurrentPages(currentPage + 1);
+  }
 
-    console.log(currentPage)
+  function handleClickPrev() {
+    setCurrentPages(currentPage - 1);
+  }
 
-    function handleClickNext(page:number){
-      setCurrentPage(page)
+  function handleClickPage(page: number) {
+    setCurrentPages(page);
+  }
+
+  function renderPageButtons() {
+    const pageButtons = [];
+
+    const maxButtons = 9;
+    const leftEllipsis = currentPage - 4 > 1;
+    const rightEllipsis = currentPage + 4 < totalPages;
+
+    let start = currentPage - 4;
+    let end = currentPage + 4;
+
+    if (start < 1) {
+      end += Math.abs(start) + 1;
+      start = 1;
     }
 
-    function renderPageButtons(){
-      const pageButton = []
-
-      for(let i=1; i<= 20; i++){
-        pageButton.push(
-          <button key={i} onClick={() => handleClickNext(i)}>{i}</button>
-        )
-      }
-      return pageButton
+    if (end > totalPages) {
+      start -= end - totalPages;
+      end = totalPages ;
     }
+
+    for (let i = start; i <= end; i++) {
+      pageButtons.push(
+        <PageButton key={i} isActive={i === currentPage} onClick={() => handleClickPage(i)}>
+          {i}
+        </PageButton>
+      );
+    }
+
+    if (leftEllipsis) {
+
+      pageButtons.unshift(<PageButton key="1" onClick={() => handleClickPage(1)}>1</PageButton>);
+    }
+
+
+    return pageButtons;
+  }
 
   return (
-  <div>
-    {renderPageButtons()}
-  </div>
+    <Wrapper>
+      <PaginationArrowButton disabled={currentPage === 1} onClick={handleClickPrev}>
+        &laquo;
+      </PaginationArrowButton>
+      {renderPageButtons()}
+      <PaginationArrowButton disabled={currentPage === totalPages} onClick={handleClickNext}>
+        &raquo;
+      </PaginationArrowButton>
+    </Wrapper>
   );
 }
